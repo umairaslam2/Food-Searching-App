d38f922fbf7a09f3298b8b802c11283cdaa4bfb3
Muhammad Umair Aslam <umairaslamumer@gmail.com>
2025-04-06 23:49:51 +0500

    Update Navbar.jsx

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -6,14 +6,14 @@ function Navbar() {
      px-4 bg-[#F8EFBA] py-4 shadow-md'>
         <div className="left">
             <div className="logo font-bold text-2xl text-black text-center">
-            Food App</div>
+            Food App By Umair</div>
         </div>        
         <div className="right">
             <ul className='flex space-x-4 text-black justify-center'>
                 <li className='cursor-pointer'>Home</li>
                 <li className='cursor-pointer'>About</li>
                 <li className='cursor-pointer'>Contact</li>
-                <li className='cursor-pointer'>Services</li>
+                <li className='cursor-pointer'>Logout</li>
             </ul>
         </div>
     </div>
